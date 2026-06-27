ca90099f03d9b30b120f1180670b8efe5e7b6563
benoitmondjo <benoitmondjo@gmail.com>
2026-06-28 00:10:07 +0100

    initialisation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "login",
+};
+
+export default function RootLayout({ children }: Readonly<{children: React.ReactNode; }>) 
+{
+    return (
+        <html lang="fr" >
+            <body   
+                >
+                    
+                {children}
+            </body>
+        </html>
+    )
+
+}
